f1e5ca1d46940baf32ad6937bba22b1dc3888470
pleed0215 <pleed0215@icloud.com>
2020-11-01 14:17:59 +0900

    profile jobs

--- screens/Main/Profile.js
+++ screens/Main/Profile.js
@@ -1,15 +1,65 @@
 import React, { useEffect, useState } from "react";
-import {ActiveIndicator} from "react-native";
+import { ActiveIndicator } from "react-native";
 import { connect } from "react-redux";
 import styled from "styled-components/native";
 import { getProfile } from "../../redux/usersSlice";
+import utils from "../../utils";
+import ThemeColor from "../../color";
 
 const Container = styled.View`
-  justify-content: center;
+  width: 100%;
+  height: ${utils.screenHeight / 3}px;
+  padding: 10px;
+
+  justify-content: flex-start;
   align-items: center;
   flex: 1;
 `;
+
+const ContainerImage = styled.ImageBackground`
+  width: ${utils.screenWidth - 10}px;
+
+  height: ${utils.screenHeight / 3}px;
+  resize-mode: cover;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
 const Text = styled.Text``;
+
+const ProfileContainer = styled.View`
+  position: absolute;
+  top: 150px;
+  background-color: rgba(255, 255, 255, 0.9);
+  width: 100%;
+  height: 300px;
+  align-items: center;
+  border-radius: 10px;
+`;
+const AvatarContainer = styled.View`
+  position: absolute;
+  top: -60px;
+  width: 120px;
+  height: 120px;
+  border-radius: 100px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${ThemeColor.grey};
+  box-shadow: 2px 3px 3px rgba(50, 50, 50, 0.8);
+`;
+const AvatarInitial = styled.Text`
+  color: white;
+  font-size: 25px;
+  font-weight: 600;
+  text-transform: uppercase;
+`;
+
+const AvatarImage = styled.Image`
+  width: 150px;
+  height: 150px;
+`;
 
 function mapStateToProps(state) {
   return { profile: state.usersReducer.profile };
@@ -23,14 +73,28 @@ const Profile = ({ profile, getProfile }) => {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     if (!profile) {
-      setLoading(true);
       getProfile();
-      setLoading(false);
-      console.log(profile);
     }
   }, []);
-return <Container>{profile && (loading?<ActiveIndicator />:<Text>{profile.username}</Text>)}</Container>
-    
-  
+  return (
+    <Container>
+      <ContainerImage source={utils.defaultImage}>
+        {profile &&
+          (loading ? (
+            <ActiveIndicator />
+          ) : (
+            <ProfileContainer>
+              <AvatarContainer>
+                {profile.avatar ? (
+                  <AvatarImage source={{ uri: profile.avatar.file }} />
+                ) : (
+                  <AvatarInitial>{profile.username.slice(0, 2)}</AvatarInitial>
+                )}
+              </AvatarContainer>
+            </ProfileContainer>
+          ))}
+      </ContainerImage>
+    </Container>
+  );
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
